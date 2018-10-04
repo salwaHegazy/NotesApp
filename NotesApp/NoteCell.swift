2a0dcc76736e0c6573e0dd94b63749360d2764fb
Salwa Hegazy <salwahegazy@192.168.117.128>
2018-10-04 02:50:07 +0200

    first commit

---
+++ NotesApp/NoteCell.swift
@@ -0,0 +1,24 @@
+//
+//  NoteCell.swift
+//  NotesApp
+//
+//  Created by Salwa Hegazy on 7/12/18.
+//  Copyright © 2018 Salwa Hegazy. All rights reserved.
+//
+
+import UIKit
+
+class NoteCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
